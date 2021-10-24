18d0eafb6113e35f21efd134e26bf33cb6fe1434
Neil Zhao <neil@zzrcxb.me>
2021-10-24 16:23:30 -0500

    updated the script, include peak finder

--- runner.py
+++ runner.py
@@ -4,24 +4,20 @@ import logging
 import numpy as np
 import subprocess
 
-from time import sleep
 from random import sample
-from typing import Iterable
+from typing import Iterable, List
 from subprocess import PIPE
 from matplotlib import pyplot as plt
 from collections import defaultdict
+from numpy.core.fromnumeric import sort
+from scipy.signal import find_peaks
+from scipy import ndimage
 
 SMT_CONTEXTS_CNT = 2
 BINO_BIN = 'bin/bino'
 
 
-def reject_outliers(data, m=2):
-    mean = np.mean(data)
-    std = np.std(data)
-    return list(filter(lambda d: abs(d - mean) < m * std, data))
-
-
-def scan_ht_cores():
+def scan_ht_cores() -> List[str]:
     results = defaultdict(set)
     line_of_interests = ['processor', 'physical id', 'core id']
     ids = []
@@ -40,9 +36,51 @@ def scan_ht_cores():
             filter(lambda t: len(t) == SMT_CONTEXTS_CNT, results.values())]
 
 
-def plotter(name, offsets: Iterable[int], means: Iterable[float], stds: Iterable[float]):
+def top_n_peaks(peaks: np.ndarray, means: np.ndarray, npeaks: int,
+                reverse: bool) -> np.ndarray:
+    peaks = sorted(peaks, key=lambda x: means[x], reverse=reverse)
+    return peaks[-npeaks:]
+
+
+def peak_seeking(means: np.ndarray, npeaks: int, reverse: bool=True):
+    data = -means if reverse else means
+    peaks, _ = find_peaks(data, threshold=np.mean(means) * .01)
+    return top_n_peaks(peaks, means, npeaks, reverse)
+
+
+def peak_seeking_std(means: np.ndarray, npeaks: int, sigma: int=2,
+                     reverse: bool = True):
+    data = -means if reverse else means
+    mean = np.mean(data)
+    std = np.std(data)
+    image = data > (mean + sigma * std)
+    regions = ndimage.find_objects(ndimage.label(image)[0])
+    peaks = np.array([np.argmax(data[r[0]]) + r[0].start for r in regions])
+    return top_n_peaks(peaks, means, npeaks, reverse)
+
+
+def plotter(name, offsets: np.ndarray, means: np.ndarray,
+            stds: np.ndarray, args, plot_std: bool = True):
     fig, ax = plt.subplots()
     ax.plot(offsets, means, color='blue')
+
+    avg = np.mean(means)
+    std = np.std(means)
+    ax.axhline(y=avg, color='orange', linestyle='dashed', linewidth=1)
+    ax.axhline(y=avg + args.sigma * std, color='grey', linestyle='dashed', linewidth=.5)
+    ax.axhline(y=avg - args.sigma * std, color='grey', linestyle='dashed', linewidth=.5)
+
+    if plot_std:
+        ub = means + stds
+        lb = means - stds
+        ax.fill_between(offsets, lb, ub, alpha=.5)
+
+    peaks = peak_seeking_std(means, args.npeaks, sigma=args.sigma)
+    ax.plot(offsets[peaks], means[peaks], 'rx')
+    for p in peaks:
+        ax.annotate(f'{offsets[p]:#x}', (offsets[p], means[p]),
+                    textcoords="offset points", xytext=(0, -15),
+                    ha='center', fontsize=9)
 
     min_x, max_x = min(offsets), max(offsets)
     interval_x = (max_x - min_x) // 16
@@ -51,7 +89,7 @@ def plotter(name, offsets: Iterable[int], means: Iterable[float], stds: Iterable
     ax.set_xticks(np.arange(min_x, max_x, interval_x))
     ax.set_xticklabels([f'{int(x):#x}' for x in ax.get_xticks()])
     ax.set_xlabel('offset')
-    ax.set_ylabel('measure')
+    ax.set_ylabel(f'avg. measure ({args.iter} runs)')
     ax.tick_params(labelsize=9)
 
     fig.text(.5, 1, name, ha='center', rotation='horizontal', fontsize=12)
@@ -75,8 +113,10 @@ def repeat_runner(name: str, cores: str, args):
     valid_cnt, run_cnt = 0, 0
     while valid_cnt < args.iter:
         logging.info(f'Run: {run_cnt}...')
-        p = subprocess.run(['taskset', '-c', cores, BINO_BIN, name],
-                           stderr=PIPE, stdout=PIPE)
+        cmd = ['taskset', '-c', cores, BINO_BIN, name]
+        if args.sudo:
+            cmd = ['sudo'] + cmd
+        p = subprocess.run(cmd, stderr=PIPE, stdout=PIPE)
         offsets, hist = [], []
         out = p.stdout.decode('utf8')
         for line in out.split('\n'):
@@ -96,19 +136,12 @@ def repeat_runner(name: str, cores: str, args):
             valid_cnt += 1
         run_cnt += 1
 
-    offsets = []
-    means = []
-    stds = []
-    for offset in sorted(counts.keys()):
-        mean = np.mean(counts[offset])
-        std = np.std(counts[offset])
-        offsets.append(offset)
-        means.append(mean)
-        stds.append(std)
+    offsets = np.array(sorted(counts.keys()))
+    means = np.array([np.mean(counts[o]) for o in offsets])
+    stds = np.array([np.std(counts[o]) for o in offsets])
+    plotter(name, offsets, means, stds, args)
+    for offset, mean, std in zip(offsets, means, stds):
         print(f'{offset:#x}\t{mean:.2f}\t{std:.2f}')
-
-    plotter(name, offsets, means, stds)
-
 
 if __name__ == '__main__':
     import argparse
@@ -117,29 +150,28 @@ if __name__ == '__main__':
                         help='Pin processes to these cores (e.g., "1,9")'\
                              'Will do auto detection if not specified')
     parser.add_argument('-i', '--iter', type=int, default=10)
+    parser.add_argument('-n', '--npeaks', type=int,
+                        default=4, help='# of peaks to find')
+    parser.add_argument('--avg-thresh', type=float, default=None,
+                              help='A threshold of avg(hist) to accept the data')
+    parser.add_argument('--std-thresh', type=float, default=None,
+                              help='A threshold of std(hist) to reject the data')
+    parser.add_argument('-S', '--sudo', action='store_true',
+                        help='Running it as sudo so it can set priorty to 0,'
+                             'make sure you don\'t need passwd for sudo, or it'
+                             'has been cached')
+    parser.add_argument('--sigma', type=int, default=2, help='Sigma for the filter')
 
     subparsers = parser.add_subparsers(dest='command')
     store_sender = subparsers.add_parser('store_offset')
-    store_sender.add_argument('--avg-thresh', type=float, default=75,
-                              help='A threshold of avg(hist) to accept the data')
-    store_sender.add_argument('--std-thresh', type=float, default=20,
-                              help='A threshold of std(hist) to reject the data')
-    store_sender.set_defaults(name='store_offset')
+    store_sender.set_defaults(
+        name='store_offset', npeaks=1, avg_thresh=75, std_thresh=20)
 
     load_trp = subparsers.add_parser('load_page_throughput')
-    load_trp.add_argument('--avg-thresh', type=float, default=None,
-                              help='A threshold of avg(hist) to accept the data')
-    load_trp.add_argument('--std-thresh', type=float, default=None,
-                              help='A threshold of std(hist) to reject the data')
     load_trp.set_defaults(name='load_page_throughput')
 
     load_ctt = subparsers.add_parser('load_page_contention')
-    load_ctt.add_argument('--avg-thresh', type=float, default=None,
-                              help='A threshold of avg(hist) to accept the data')
-    load_ctt.add_argument('--std-thresh', type=float, default=None,
-                              help='A threshold of std(hist) to reject the data')
     load_ctt.set_defaults(name='load_page_contention')
-
 
     args = parser.parse_args()
 
