e9df75b329c7bcf35160f1590ee7ec8c971bd555
Neil Zhao <neil@zzrcxb.me>
2022-02-11 03:18:29 -0600

    more helper functions for the overhual

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sched.h>
+#include <unistd.h>
+
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/resource.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -21,6 +28,7 @@ typedef float f32;
 typedef double f64;
 
 #define ALWAYS_INLINE __attribute__((always_inline))
+#define UNUSED __attribute__((unused))
 
 // assembly related
 #define _maccess(P)                                                            \
@@ -56,75 +64,103 @@ static inline ALWAYS_INLINE u64 _rdtscp(u32 *aux) {
     return (rdx << 32) + rax;
 }
 
-#define SEL_NOSPEC(MASK, T, F)                                                 \
-    (((MASK) & (typeof(MASK))(T)) | (~(MASK) & (typeof(MASK))(F)))
+static inline ALWAYS_INLINE u64 _rdtsc_google_begin(void) {
+    u64 t;
+    asm volatile("lfence\n\t"
+                 "rdtsc\n\t"
+                 "shl $32, %%rdx\n\t"
+                 "or %%rdx, %0\n\t"
+                 "lfence"
+                 : "=a"(t)
+                 :
+                 // "memory" avoids reordering. rdx = TSC >> 32.
+                 // "cc" = flags modified by SHL.
+                 : "rdx", "memory", "cc");
+    return t;
+}
 
-// timing related
-#define _TIMEIT(TYPE, P)                                                       \
+static inline ALWAYS_INLINE u64 _rdtscp_google_end(void) {
+    u64 t;
+    asm volatile("rdtscp\n\t"
+                 "shl $32, %%rdx\n\t"
+                 "or %%rdx, %0\n\t"
+                 "lfence"
+                 : "=a"(t)
+                 :
+                 // "memory" avoids reordering. rcx = TSC_AUX. rdx = TSC >> 32.
+                 // "cc" = flags modified by SHL.
+                 : "rcx", "rdx", "memory", "cc");
+    return t;
+}
+
+#define _timer_start _rdtsc_google_begin
+#define _timer_end   _rdtscp_google_end
+
+// page table related functions
+// assuming 4KB page and 4-level PT
+#define PAGE_SHIFT (12u)
+#define PAGE_SIZE (1u << PAGE_SHIFT)
+#define INDEX_WIDTH (9u)
+#define INDEX_COUNT (1u << INDEX_WIDTH)
+#define INDEX_MASK (INDEX_COUNT - 1)
+
+static inline ALWAYS_INLINE u64 addr_crafter(u64 pl4, u64 pl3, u64 pl2, u64 pl1) {
+    u64 page = (pl4 << 27) + (pl3 << 18) + (pl2 << 9) + pl1;
+    return page << 12;
+}
+
+// get index for a given pointer and a level of page table
+// PGD (level 4) -> PUD (level 3) -> PMD (level 2) -> PTE (level 1)
+static inline ALWAYS_INLINE u16 get_PL_index(void *ptr, u8 level) {
+    u64 page = ((uintptr_t)ptr >> PAGE_SHIFT);
+    return (page >> ((level - 1) * 9)) & 0x1ff;
+}
+
+// mmap related
+static u8 *mmap_private(void *addr, u64 size) {
+    u8 *ptr = mmap(addr, size, PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (ptr == MAP_FAILED)
+        return NULL;
+    return ptr;
+}
+
+static u8 *mmap_private_init(void *addr, u64 size, u8 init) {
+    u8 *ptr = mmap_private(addr, size);
+    if (ptr) {
+        memset(ptr, init, size);
+    }
+    return ptr;
+}
+
+static u8 *mmap_shared(void *addr, u64 size) {
+    u8 *ptr = mmap(addr, size, PROT_READ | PROT_WRITE,
+                   MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (ptr == MAP_FAILED)
+        return NULL;
+    return ptr;
+}
+
+static u8 *mmap_shared_init(void *addr, u64 size, u8 init) {
+    u8 *ptr = mmap_shared(addr, size);
+    if (ptr) {
+        memset(ptr, init, size);
+    }
+    return ptr;
+}
+
+// affinity
+static bool set_affinity_priority(uint core, int prio) {
+    cpu_set_t set;
+    CPU_ZERO(&set);
+    CPU_SET(core, &set);
+    bool ret1 = sched_setaffinity(getpid(), sizeof(set), &set) != -1;
+    bool ret2 = setpriority(PRIO_PROCESS, 0, prio) != -1;
+    return ret1 && ret2;
+}
+
+// error message related
+#define _error(...)                                                            \
     do {                                                                       \
-        t_start = _rdtscp(&_sig);                                              \
-        _maccess((TYPE *)P);                                                   \
-        t_end = _rdtscp(&_sig);                                                \
-    } while (0);
-
-static uint32_t _time_maccess(void *p, size_t size) {
-    unsigned int _sig;
-    uint64_t t_start, t_end;
-
-    switch (size) {
-    case 1:
-        _TIMEIT(uint8_t, p);
-        break;
-    case 2:
-        _TIMEIT(uint16_t, p);
-        break;
-    case 4:
-        _TIMEIT(uint32_t, p);
-        break;
-    case 8:
-        _TIMEIT(uint64_t, p);
-        break;
-    default:
-        assert(false && "Unsupported size");
-    }
-    return t_end - t_start;
-}
-
-static unsigned _get_cache_hit_threshold(void) {
-    unsigned const size = 4096;
-    unsigned const count = 1000;
-    size_t i;
-    unsigned threshold;
-    uint64_t hit_sum = 0, miss_sum = 0;
-
-    char *data = malloc(size);
-    if (!data)
-        return 0;
-    memset(data, 0, size);
-
-    // warmup
-    for (i = 0; i < count; i++) {
-        _maccess(data);
-    }
-
-    // measure hits
-    for (i = 0; i < count; i++) {
-        hit_sum += _time_maccess(data, 1);
-    }
-
-    for (i = 0; i < count; i++) {
-        _clflush(data);
-        _mfence();
-        miss_sum += _time_maccess(data, 1);
-    }
-
-    free(data);
-    threshold = (miss_sum + 3 * hit_sum) / (4 * count);
-    if (threshold > 150) {
-        fprintf(stderr,
-                "[WARN] Cache hit threshold is abnormally high at %u cycles, "
-                "please check your system settings.\n",
-                threshold);
-    }
-    return threshold;
-}
+        fprintf(stderr, __VA_ARGS__);                                          \
+    } while (0)
